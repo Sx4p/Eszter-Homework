231b33bb7e402361101a4d1fa6d383e7f0642e5a
erdelyieszter <esztergyerdelyi@gmail.com>
2023-10-21 12:52:21 +0200

    create React browser router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import React from "react";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Table from "./pages/Table";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <NavBar/>,
+    children: [
+      {
+        path: "/",
+        element: <Table/>
+      }
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+  {
+    path: "/register",
+    element: <Register/>
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-    </div>
+      <RouterProvider router={router}/>
   );
 }
 
