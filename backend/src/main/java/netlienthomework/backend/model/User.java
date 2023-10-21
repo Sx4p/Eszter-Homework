75d71cb184abef6609bc128dbec7e4ffa7a707f4
erdelyieszter <esztergyerdelyi@gmail.com>
2023-10-21 11:38:43 +0200

    add table name to User entity

--- backend/src/main/java/netlienthomework/backend/model/User.java
+++ backend/src/main/java/netlienthomework/backend/model/User.java
@@ -1,9 +1,6 @@
 package netlienthomework.backend.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -13,6 +10,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "\"user\"")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
