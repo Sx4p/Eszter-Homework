1d4d0519c63ac174637610162f67c6e2c2528da2
erdelyieszter <esztergyerdelyi@gmail.com>
2023-10-21 11:42:04 +0200

    create getAllProducts endpoint

---
+++ backend/src/main/java/netlienthomework/backend/controller/ProductController.java
@@ -0,0 +1,27 @@
+package netlienthomework.backend.controller;
+
+import netlienthomework.backend.model.Product;
+import netlienthomework.backend.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/product")
+public class ProductController {
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
